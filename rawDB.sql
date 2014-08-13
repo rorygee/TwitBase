dc0cff52b997be53289f1e9262794219a5467697
rorygee <rorygee@outlook.com>
2014-08-13 11:13:37 +0100

    Attempt at saving DB ready for collaboration

---
+++ rawDB.sql
@@ -0,0 +1,14 @@
+﻿-- Database: "TwitBase"
+
+-- DROP DATABASE "TwitBase";
+
+CREATE DATABASE "TwitBase"
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'English_United States.1252'
+       LC_CTYPE = 'English_United States.1252'
+       CONNECTION LIMIT = -1;
+
+COMMENT ON DATABASE "TwitBase"
+  IS 'DB for Twitter scooper';
